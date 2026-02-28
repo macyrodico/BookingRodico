a2bb11c46c378ce2d8d6451c71e887b8af192176
macyrodico <mraynerodico@gmail.com>
2026-02-28 16:01:33 +0800

    added add and view booking function

--- Program.cs
+++ Program.cs
@@ -1,71 +1,134 @@
 ﻿
-using System.ComponentModel.Design;
+using System;
+
 
 namespace BookingRodico
 {
     internal class Program
     {
+
+
+        static string[] passengerName = new string[10];
+        static string[] destination = new string[10];
+        static string[] baggageWeight = new string[10];
+        static string[] mealAmount = new string[10];
+
+        static int count = 0;
+
         static void Main(string[] args)
+
         {
-            Console.WriteLine("--- WELCOME TO FLIGHT BOOKING SYSTEM ---");
 
-
-            Console.WriteLine("Choose a Booking type:");
-            Console.WriteLine("1. Public");
-            Console.WriteLine("2. Private");
-
-            string choice = Console.ReadLine();
-
-
-            if (choice == "1")
+            while (true) 
             {
-                PublicBooking();
 
+
+                Console.WriteLine("--- WELCOME TO FLIGHT BOOKING SYSTEM ---");
+
+
+                Console.WriteLine("Choose a number:");
+                Console.WriteLine("1. Add Booking");
+                Console.WriteLine("2. View Booking");
+                Console.WriteLine("3. Update Booking");
+
+                string choice = Console.ReadLine();
+
+
+                if (choice == "1")
+                    AddBooking();
+
+                else if (choice == "2")
+                    ViewBooking();
+
+                else if (choice == "3")
+                    //UpdateBooking();
+
+                else
+                    Console.WriteLine("Invalid choice. Please try again.");
             }
-            else if (choice == "2")
-            {
-                PrivateBooking();
-
-            } else
-            {
-                Console.WriteLine("Invalid choice. Please select either 1 or 2.");
-            }
+        }
+    
 
 
-
-                static void PublicBooking()
+                static void AddBooking()
                 {
-                    Console.WriteLine("--- Public Booking. ---");
-                    Console.Write("Enter your destination: ");
-                    string destination = Console.ReadLine();
-
-                    Console.Write("Enter your departure date: ");
-                    string departureDate = Console.ReadLine();
-
-                    Console.Write("Enter Passenger name: ");
-                    string name = Console.ReadLine();
-
-                    Console.Write("Meal Preference: ");
-                    string mealPreference = Console.ReadLine();
-
-                    Console.WriteLine("Baggage (kg): ");
-                    string baggage = Console.ReadLine();
 
 
-                }
+                if (count >= 10)
+                {
 
-            static void PrivateBooking()
-            {
-                Console.WriteLine("--- Public Booking. ---");
-                Console.Write("Enter your destination: ");
-                string destination = Console.ReadLine();
+                Console.WriteLine("Booking list is full!");
 
-                Console.Write("Enter your departure date: ");
-                string departureDate = Console.ReadLine();
+            }
+
 
                 Console.Write("Enter Passenger name: ");
-                string name = Console.ReadLine();
+                passengerName[count] = Console.ReadLine();
+
+                Console.Write("Enter destination: ");
+                destination[count] = Console.ReadLine();
+
+
+                Console.WriteLine("Baggage Weight (Max is 20 kg): ");
+                int baggage = Convert.ToInt32(Console.ReadLine());
+
+
+                if (baggage > 20)
+                {
+
+                Console.WriteLine("Baggage limit exceeded! Max weight is 20 kg.");
+
             }
+
+
+                Console.Write("Meal Amount (Max is 2): ");
+                int meal = Convert.ToInt32(Console.ReadLine());
+
+
+                if (meal > 2)
+                {
+
+                Console.WriteLine("Meal limit exceeded! Max is 2.");
+
+            }
+
+                baggageWeight[count] = baggage.ToString();
+                mealAmount[count] = meal.ToString();
+
+                count++;
+
+                Console.WriteLine("Booking Added Successfully!");
+
+
+        }
+
+            static void ViewBooking()
+            {
+
+            
+            if (count == 0)
+            {
+                Console.WriteLine("No bookings available.");
+
+            }
+
+
+            for (int i = 0; i < count; i++)
+            {
+
+                Console.WriteLine("Booking no. " + (i + 1));
+                Console.WriteLine("Passenger name: " + passengerName[i]);
+
+                Console.WriteLine("Destination: " + destination[i]);
+
+                Console.WriteLine("Baggage: " + baggageWeight[i] + "kg");
+
+                Console.WriteLine("Meals: " + mealAmount[i]);
+
+
+            }
+                
+               
 
             }
         }
